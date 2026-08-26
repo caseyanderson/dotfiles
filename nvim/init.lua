96ecf1950e638e1649e135bc71c56f6d92dce925
caseyanderson <cta@caseyanderson.com>
2026-08-25 22:24:15 -0700

    bootstrap lazy.nvim and add lualine

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,3 +5,36 @@ vim.opt.relativenumber = true
 vim.opt.mouse = "a"
 vim.opt.termguicolors = true
 
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  local result = vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+
+  if vim.v.shell_error ~= 0 then
+    error("Could not install lazy.nvim:\n" .. result)
+  end
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local init_file = debug.getinfo(1, "S").source:sub(2)
+local config_dir = vim.fn.fnamemodify(init_file, ":p:h")
+
+require("lazy").setup({
+  spec = {
+    {
+      "nvim-lualine/lualine.nvim",
+      opts = {},
+    },
+  },
+  lockfile = config_dir .. "/lazy-lock.json",
+  checker = { enabled = false },
+  change_detection = { notify = false },
+})
