0dd510007913baa1941e33e82b757f01a809410a
caseyanderson <cta@caseyanderson.com>
2026-08-25 23:01:18 -0700

    modernize core Neovim plugins

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,6 +1,60 @@
 return {
   {
+    "phha/zenburn.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("zenburn").setup()
+      vim.cmd.colorscheme("zenburn")
+    end,
+  },
+
+  {
     "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      options = {
+        theme = "zenburn",
+      },
+    },
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    keys = {
+      {
+        "<C-n>",
+        "<cmd>NvimTreeToggle<cr>",
+        desc = "Toggle file tree",
+      },
+    },
+    opts = {
+      view = {
+        width = 30,
+      },
+      renderer = {
+        group_empty = true,
+      },
+    },
+  },
+
+  {
+    "kylechui/nvim-surround",
+    version = "^4.0.0",
+    event = "VeryLazy",
     opts = {},
+  },
+
+  {
+    "tpope/vim-fugitive",
+    cmd = {
+      "Git",
+      "G",
+    },
   },
 }
